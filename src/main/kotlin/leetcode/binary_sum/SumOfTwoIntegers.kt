2bdb7740380b5bcafeb3b0408564d428567ed09f
Pawlllosss <oczadlypawe@gmail.com>
2023-01-02 19:20:20 +0100

    Sum of two integers

---
+++ src/main/kotlin/leetcode/binary_sum/SumOfTwoIntegers.kt
@@ -0,0 +1,34 @@
+package leetcode.binary_sum
+
+fun main() {
+    println("${getSum(2, 3)} ${binarySum(2, 3)}")
+    println("${getSum(-2, -3)} ${binarySum(-2, -3)}")
+}
+
+fun getSum(a: Int, b: Int): Int {
+    var d = a
+    val loopPositions = if (b > 0) b else -b
+
+    for (i in 0 until loopPositions) {
+        if (b > 0) {
+            d++
+        } else {
+            d--
+        }
+    }
+
+    return d
+}
+
+fun binarySum(a: Int, b: Int): Int {
+    var xorResult = a xor b
+    var carryResult = a and b
+
+    while (carryResult != 0) {
+        val shiftedCarry = carryResult shl 1
+        carryResult = shiftedCarry and xorResult
+        xorResult = xorResult xor shiftedCarry
+    }
+
+    return xorResult
+}
