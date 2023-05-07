c1ce6036ffdefe697603e4d60e1143aa53ac5e88
Paweł Oczadly <oczadlypawe@gmail.com>
2023-05-07 16:58:06 +0200

    Longest substring without repeating char (#8)
    * longest substring without repeating characters
    
    * added test case, improved performance by using index instead of new substring
    
    * another approach which relies on sliding window and indexes

---
+++ src/test/kotlin/leetcode/norepeating/LongestSubstringWithoutRepetitionKtTest.kt
@@ -0,0 +1,35 @@
+package leetcode.norepeating
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+internal class LongestSubstringWithoutRepetitionKtTest {
+
+    companion object {
+        @JvmStatic
+        fun stringWithResult(): Stream<Arguments> = Stream.of(
+            Arguments.of("", 0),
+            Arguments.of("abcabcbb", 3),
+            Arguments.of("bbbbbb", 1),
+            Arguments.of("pwwkew", 3),
+            Arguments.of("a3a32a3", 3),
+            Arguments.of("aab", 2),
+            Arguments.of("loddktdji", 5),
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringWithResult")
+    fun shouldReturnLengthOfLongestSubstringWithoutRepeatingCharacters(string: String, expectedResult: Int) {
+        Assertions.assertThat(lengthOfLongestSubstring(string)).isEqualTo(expectedResult)
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringWithResult")
+    fun shouldReturnLengthOfLongestSubstringWithoutRepeatingCharacters2(string: String, expectedResult: Int) {
+        Assertions.assertThat(lengthOfLongestSubstringWithIndexes(string)).isEqualTo(expectedResult)
+    }
+}
